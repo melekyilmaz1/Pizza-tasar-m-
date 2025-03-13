476487ee7b3067aea1b5de3a02a59fc032d8a747
Melek <melekyilmaz2307@gmail.com>
2025-03-13 19:57:20 +0300

    Initial Commit

---
+++ tasarımTests/tasar_mTests.swift
@@ -0,0 +1,17 @@
+//
+//  tasar_mTests.swift
+//  tasarımTests
+//
+//  Created by Melek Yılmaz on 13.03.2025.
+//
+
+import Testing
+@testable import tasar_m
+
+struct tasar_mTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
